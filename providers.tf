544d9e82a4a766f9228de022d2137048819d8781
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2020-06-20 22:13:20 -0600

    Updated gorilla profile. Removing leonardo aws.

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 provider "aws" {
   region                  = "us-east-1"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "leonardo"
-  //profile                 = "gorilla"
+  profile                 = "gorilla-noise-alert-terraform-dev"
 
   allowed_account_ids = [
-    "961622453478", // leonardo cordero's account
     "002631123367"
   ]
 }
