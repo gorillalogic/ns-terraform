879d15f21afa61f3908b2a84a432b3aa092a7747
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2020-06-20 22:13:14 -0600

    Add thing policies.

--- iot.tf
+++ iot.tf
@@ -9,7 +9,8 @@ resource "aws_iot_topic_rule" "kinesis_push_rule" {
   sql_version = "2015-10-08"
 
   kinesis {
-    stream_name = "noise_sensor_stream"
+    stream_name = "mqtt-ingestor"
+    partition_key = "e4ecd436-f618-4ae5-b5bf-53742384cffe"
     role_arn = aws_iam_role.iot_kinesis_role.arn
   }
 }
